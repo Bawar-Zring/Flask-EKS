04094306c0b456a8c957a2f55ec778e6220c1c5f
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-10 07:59:07 +0300

    selecting easy mode for redis cluster

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -265,14 +265,14 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis"
   engine               = "redis"
-  node_type            = "cache.t3.micro"
+  node_type            = "cache.t4g.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis5.0"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = [aws_security_group.redis.id]  
-  
-    tags = {
-        Name = "redis"
-    }
+  security_group_ids   = [aws_security_group.redis.id]
+
+  tags = {
+    Name = "redis"
+  }
 }
