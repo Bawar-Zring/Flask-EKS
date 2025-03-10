2bf0b8da2126a77b03437f0feca4d55062c8f21c
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-10 20:43:20 +0300

    adding output to main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -330,3 +330,11 @@ resource "aws_db_instance" "postgres" {
     Name = "postgres"
   }
 }
+
+output "postgres_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
+
+output "redis_endpoint" {
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
+}
