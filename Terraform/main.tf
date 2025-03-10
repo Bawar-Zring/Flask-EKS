73c49e9df2254ad688308c9c2971a88ce7271b01
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-10 09:08:48 +0300

    change username

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -313,7 +313,7 @@ resource "aws_db_instance" "postgres" {
   storage_type         = "gp2"          
   db_subnet_group_name = aws_db_subnet_group.postgres_subnet_group.name
   vpc_security_group_ids = [aws_security_group.postgres.id]
-  username = "admin"
+  username = "admin_test"
   password = "root"
 
   skip_final_snapshot = true
